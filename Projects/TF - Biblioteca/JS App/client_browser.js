c7821d6df48c38b92f47c84835694ef571a7a445
Fernando Homem da Costa <nandohdc@gmail.com>
2017-07-02 13:49:10 -0300

    app optimization

--- Projects/TF - Biblioteca/JS App/client_browser.js
+++ Projects/TF - Biblioteca/JS App/client_browser.js
@@ -1,13 +1,6 @@
 var table_points = [{x : 73, y : 324},{x : 74, y : 445},{x : 74, y :577},{x : 206, y : 445}, {x : 206, y : 577},{x : 342, y : 312}, {x : 341,y : 445},{x : 536, y : 577},{x : 668, y : 314}, {x : 668, y : 445}, {x : 668, y : 577}];
 var table_head = ["QTDA", "MINTEMP", "MAXTEMP", "AVGTEMP", "FD", "OD"];
-table_head["QTDA"] = 0;
-table_head["MINTEMP"] = 0;
-table_head["MAXTEMP"] = 0;
-table_head["AVGTEMP"] = 0;
-table_head["FD"] = 0;
-table_head["OD"] = 0;
 var table_main=["id", "temp", "status", "posX", "posY"];
-
 var array_NodeClients = [];
 
 var canvas;
@@ -15,9 +8,6 @@ var context;
 var images = {};
 var totalResources = 5;
 var numResourcesLoaded = 0;
-var fps = 30;
-var charX = 0;
-var charY = 0;
 
 loadImage("background");
 loadImage("character01");
@@ -25,7 +15,6 @@ loadImage("character02");
 loadImage("character03");
 loadImage("character04");
 
-var clientID = Math.floor((Math.random() * 10 ) + 1);
 var hostname = 'mqtt://test.mosca.io';
 var client = mqtt.connect(hostname);
 
@@ -56,17 +45,15 @@ function redraw(){
 
 	if(canvas.getContext){
 		context = canvas.getContext("2d");
-		var x = charX;
-		var y = charY;
 		var randomImage;
 
-		canvas.width = canvas.width;
+		canvas.width = canvas.width; //limpa o canvas
 
-		context.drawImage(images["background"], x, y);
+		context.drawImage(images["background"], 0, 0);
 		for(var index = 0;  index < array_NodeClients.length; index++){
 			if(array_NodeClients[index].status.toString() === "occupied"){
-				randomImage = Math.floor(Math.random()*(3) + 1);
-				context.drawImage(images["character0"+randomImage.toString()], array_NodeClients[index].posX, array_NodeClients[index].posY);
+				randomImage = "character0" + Math.floor(Math.random()*(3) + 1);
+				context.drawImage(images[randomImage], array_NodeClients[index].posX, array_NodeClients[index].posY);
 			}
 		}
 	}
@@ -74,9 +61,6 @@ function redraw(){
 
 
 client.on('connect', function(){
-	console.log("Client - ID: " + clientID + " conectado");
-				
-	client.subscribe('nandohdc/connect');
-				
+	client.subscribe('nandohdc/connect');				
 	client.subscribe('nandohdc/infos');
 })
--- Projects/TF - Biblioteca/JS App/functions.js
+++ Projects/TF - Biblioteca/JS App/functions.js
@@ -1,9 +1,48 @@
+/**
+* Essa função receber dois parametros, ID_TAG e info. Insere o valor info no elemento em que o ID_TAG faz referencia do arquivo index.php.
+* @method insertTextTable
+* @param {String} ID_TAG
+* @param {String} info 
+* @return void
+*/
 function insertTextTable(ID_TAG, info){
 	var element = document.getElementById(ID_TAG);
 	element.innerHTML = info;
 	element.classList ? element.classList.add('text-center') : element.className += ' text-center';
 }
 
+/**
+* Essa função atualiza o numero total de dispositivos.
+* E atribui o calculo uma variavel global (table_head)
+* @method updateQTDA
+* @param {String} - nElements
+* @GLOBAL {Array} - table_head 
+* @return void
+*/
+
+function updateQTDA(nElements){
+	table_head["QTDA"] = nElements
+
+	if(nElements < 1){
+		table_head["QTDA"] = 0;
+		table_head["MINTEMP"] = 0;
+		table_head["MAXTEMP"] = 0;
+		table_head["AVGTEMP"] = 0;
+		table_head["FD"] = 0;
+		table_head["OD"] = 0;
+	}
+
+	insertTextTable("td_QTDA", table_head["QTDA"]);
+}
+
+/**
+* Essa função calcula/atualiza o numero de dispositivos em uso.
+* E atribui o calculo uma variavel global (table_head)
+* @method updateOccupiedDevices
+* @GLOBAL {Array{Array}}  - array_NodeClients
+* @GLOBAL {Array} - table_head 
+* @return void
+*/
 function updateOccupiedDevices(){
 	var tempOD = 0;
 
@@ -13,16 +52,34 @@ function updateOccupiedDevices(){
 		}
 	}
 	table_head["OD"] = tempOD;
+	insertTextTable("td_OD", table_head["OD"]);
 }
 
+/**
+* Essa função calcula/atualiza o numero de dispositivos em livres.
+* E atribui o calculo uma variavel global (table_head)
+* @method updateFreeDevices
+* @GLOBAL {Array} - table_head 
+* @return void
+*/
 function updateFreeDevices(){
 	var tempFD = 0;
 
 	tempFD = table_head["QTDA"] - table_head["OD"];
 
 	table_head["FD"] = tempFD;
+
+	insertTextTable("td_FD", table_head["FD"]);
 }
 
+/**
+* Essa função procura no array de arrays(array_NodeClients) e atualiza menor temperatura em outro array (table_head).
+* E atribui o valor a uma variavel global (table_head)
+* @method updateMinTemp
+* @GLOBAL {Array{Array}}  - array_NodeClients
+* @GLOBAL {Array} - table_head 
+* @return void
+*/
 function updateMinTemp(){
 	var tempMinTemp = 100;
 
@@ -33,8 +90,17 @@ function updateMinTemp(){
 	}
 
 	table_head["MINTEMP"] = tempMinTemp;
+	insertTextTable("td_MINTEMP", table_head["MINTEMP"]);
 }
 
+/**
+* Essa função procura no array de arrays(array_NodeClients) e atualiza maior temperatura em outro array (table_head).
+* E atribui o valor a uma variavel global (table_head)
+* @method updateMaxTemp
+* @GLOBAL {Array{Array}}  - array_NodeClients
+* @GLOBAL {Array} - table_head 
+* @return void
+*/
 function updateMaxTemp(){
 	var tempMaxTemp = 0;
 
@@ -45,54 +111,30 @@ function updateMaxTemp(){
 	}
 
 	table_head["MAXTEMP"] = tempMaxTemp;
+	insertTextTable("td_MAXTEMP", table_head["MAXTEMP"]);
 }
 
+/**
+* Essa função calcula e atualiza temperatura media.
+* E atribui o valor a uma variavel global (table_head)
+* @method updateAvgTemp
+* @GLOBAL {Array} - table_head 
+* @return void
+*/
 function updateAvgTemp(){
 	var tempAvgTemp = 0;
 	if(((table_head["MAXTEMP"] + table_head["MINTEMP"])/2) != table_head["AVGTEMP"]){
 		tempAvgTemp = ((parseFloat(table_head["MAXTEMP"]) + parseFloat(table_head["MINTEMP"]))/2);
 		table_head["AVGTEMP"] = tempAvgTemp;
-	}
-}
-
-function trataTopico(topic, message){
-	console.log("Topico eh: " + topic);
-	if(topic.toString() === "nandohdc/connect"){
-		trataMensagemConnect(message.toString());
-	} else if(topic.toString() === "nandohdc/infos"){
-		trataMensagemInfos(message.toString());
-	} else if(topic.toString() === "nandohdc/test"){
-
-	} else{
-		alert("MQTT - Tópico escolhido não está configurado!");
-		window.close();
-	}
-}
-
-function insertTableHead(table_name){
-	if(table_name === "table_head"){
-		insertTextTable("td_QTDA", table_head["QTDA"]);
-		insertTextTable("td_MINTEMP", table_head["MINTEMP"]);
-		insertTextTable("td_MAXTEMP", table_head["MAXTEMP"]);
 		insertTextTable("td_AVGTEMP", table_head["AVGTEMP"]);
-		insertTextTable("td_FD", table_head["FD"]);
-		insertTextTable("td_OD", table_head["OD"]);
 	}
 }
+
 
 function insertTableMain(table_name, index_of_array_node_clients){
  if(table_name === "table_main"){
 		var element = document.getElementById("table_main_body");
-		if(element.rows.length < 1){	
-			var newRow = element.insertRow(element.rows.length);
-			newRow.id = "id_" + array_NodeClients[index_of_array_node_clients]["id"].toString(); 
-			var cell01 = newRow.insertCell(0);
-			var cell02 = newRow.insertCell(0);
-			var cell03 = newRow.insertCell(0);
-			cell03.innerHTML = array_NodeClients[index_of_array_node_clients]["id"];
-			cell02.innerHTML = array_NodeClients[index_of_array_node_clients]["temp"];
-			cell01.innerHTML = array_NodeClients[index_of_array_node_clients]["status"];
-		} else if(element.rows.length != array_NodeClients.length){
+		if(element.rows.length < 1 || element.rows.length != array_NodeClients.length){	
 			var newRow = element.insertRow(element.rows.length);
 			newRow.id = "id_" + array_NodeClients[index_of_array_node_clients]["id"].toString(); 
 			var cell01 = newRow.insertCell(0);
@@ -110,7 +152,7 @@ function insertTableMain(table_name, index_of_array_node_clients){
 				}
 			}
 		} else{
-			alert("Porra");
+			alert("Error: insert_Table_Main - Array_NodeClients ");
 		}
 
 	} else {
@@ -119,35 +161,56 @@ function insertTableMain(table_name, index_of_array_node_clients){
 	}
 }
 
-function updateTableHead(){
+function updateTableHead(array_NodeClients_length){
+	updateQTDA(array_NodeClients_length);
 	updateOccupiedDevices();
 	updateFreeDevices();
 	updateMinTemp();
 	updateMaxTemp();
 	updateAvgTemp();
-	insertTableHead("table_head");
+
+}
+
+/**
+* Essa função receber dois parametros, topic e message.
+* A função avalia se o topic recebido é válido, caso seja, chamará a função que cuidadará daquele topic.
+* @method trataTopico
+* @param {Object} - topic
+* @param {Object} - message 
+* @return void
+*/
+function trataTopico(topic, message){
+	console.log("Topico eh: " + topic);
+	if(topic.toString() === "nandohdc/connect"){
+		trataMensagemConnect(message.toString());
+	} else if(topic.toString() === "nandohdc/infos"){
+		trataMensagemInfos(message.toString());
+	} else if(topic.toString() === "nandohdc/test"){
+
+	} else{
+		alert("MQTT - Tópico escolhido não está configurado!");
+		window.close();
+	}
 }
 
 function trataMensagemConnect(message){
 	console.log("Mensgem eh: " + message);
 	var nodeClient = ["id", "temp", "status", "posX", "posY"];
+	
 	nodeClient["id"] = message.toString(0);
 	nodeClient["temp"] = "0";
-	nodeClient["status"]= "INDISPONIVEL";
+	nodeClient["status"]= "free";
+	
 	if(array_NodeClients.length < 1){
 		nodeClient["posX"] = table_points[0].x;
 		nodeClient["posY"] = table_points[0].y;
-		array_NodeClients.push(nodeClient);
-		table_head["QTDA"] = array_NodeClients.length;
-		insertTableHead("table_head");
 	} else{
 		nodeClient["posX"] = table_points[array_NodeClients.length].x;
 		nodeClient["posY"] = table_points[array_NodeClients.length].y;
-		array_NodeClients.push(nodeClient);
-		console.log(array_NodeClients.length);
-		table_head["QTDA"] = array_NodeClients.length;
-		insertTableHead("table_head");
 	}
+	
+	array_NodeClients.push(nodeClient);
+	updateTableHead(array_NodeClients.length);
 }
 
 function trataMensagemInfos(message){
@@ -159,8 +222,6 @@ function trataMensagemInfos(message){
 	}
 	for(var index = 0;  index < array_NodeClients.length; index++){
 		if(array_NodeClients[index]["id"].toString() === splittedMsg[0].toString()){
-			console.log(array_NodeClients[index]["id"].toString());
-			console.log(splittedMsg[0].toString());
 			array_NodeClients[index]["status"] = splittedMsg[1].toString();
 			array_NodeClients[index]["temp"] = splittedMsg[2].toString();
 			insertTableMain("table_main", index);
