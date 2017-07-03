2afe9fbc575d7bc1f6e7a41b089d810bd074f331
Fernando Homem da Costa <nandohdc@gmail.com>
2017-07-02 23:17:32 -0300

    fixing

---
+++ Projects/TF - Biblioteca/Nodemcu/connect_JS.lua
@@ -0,0 +1,51 @@
+--Configuracoes do nodemcu para ser identificado numa rede
+nodemcu = {
+    ID = 0,
+    wificonfig = {
+        --Colocar em SSID a rede desejada para conectar
+        ssid = "Homem's Wi-Fi",
+        pwd = "02051993",
+        save = false
+    },
+    MQTT_SERVER = 'test.mosca.io',
+    Status = "free"
+}
+
+
+--Definindo LEDS
+LD1 = 6;
+LD2 = 3;
+
+function LED(led_pin)
+    local pin = led_pin
+    return {
+        inicia = function()
+                    gpio.mode(pin, gpio.OUTPUT)
+                    gpio.write(pin, gpio.LOW)
+                 end,
+        liga = function()
+                print("O NodeMCU #"..nodemcu.ID.." Led #"..pin.." Ligado")
+                gpio.write(pin, gpio.HIGH)
+               end,
+        desliga = function()
+                    gpio.write(pin, gpio.LOW)
+                  end
+    }
+end
+
+-- Conexao na rede Wifi
+wifi.setmode(wifi.STATION)
+--Nao alterar a linha de configurar wifi
+wifi.sta.config(nodemcu.wificonfig)
+wifi.sta.connect()
+
+if(wifi.sta.status() == 5) then
+    nodemcu.ID = wifi.sta.getmac()
+    print("O NodeMCU #"..nodemcu.ID.." is connected to "..nodemcu.wificonfig.ssid)
+    local led_green = LED(LD1)
+    local led_red = LED(LD2)
+    led_green.inicia()
+    led_red.inicia()
+    led_red.desliga()
+    led_green.liga()
+end
